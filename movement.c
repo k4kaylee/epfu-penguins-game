ced69fe16ae7bb36af29eed72274e0525a4c8c0d
k4kaylee <roman.kulpekshark@gmail.com>
2023-03-06 16:01:12 +0100

    Adding necessary comments

--- movement.c
+++ movement.c
@@ -1,3 +1,20 @@
-void makeAMove() {
-	
+#include "player.h"
+
+
+
+int checkMove() {
+	//checks whether move was done correctly
+	//returns '1' when all conditions are met, otherwisw returns '0'
+	//CONDITIONS:
+	//	!there is no '0' cell in the way of movement direction
+	//	!the route to a given coordinate is a straight line
+	//  !*there is no penguins on the way
+	return 0;
+}
+
+void makeAMove(/*struct Player player, char** board, int size*/) {
+	//moves given player from its initial position to available place in the board
+	if (checkMove()) {
+
+	}
 };
